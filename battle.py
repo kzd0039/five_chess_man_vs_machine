ecd54d25f249be964aa43f8c6a442c196b546745
丁坤 <dingkun@dingkundeMBP.attlocal.net>
2020-11-05 22:33:17 -0500

    correct

--- battle.py
+++ battle.py
@@ -9,7 +9,8 @@ def main():
     # black = 2, white = 1, null = 0
     board = [[0 for n in range(15)] for n in range(15)]
     isBlack = True
-    
+    chessCount = 0
+
     #base line battle
     #candidate: alpha_beta_prouning, greedy, heuristic, minimax
     #target: find the one with the best success rate
@@ -22,8 +23,11 @@ def main():
     #1: number of white wins(second algorithm, minimax here), 
     #2: number of black wins(first algorith, greedy here),
     #-1: number of draws
+    
     record = {1:0, 2:0, -1:0}
     for i in range(100):
+        # reset all the parameter
+        board = [[0 for n in range(15)] for n in range(15)]
         isBlack = True
         chessCount = 0
         while (True):
